3185d9b85a7f10b516aaa7ff92054f2936b7d146
oscherrero <36818091+oscherrero@users.noreply.github.com>
2025-03-05 22:17:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -57,19 +57,23 @@ async function cargarHTML() {
     })
 
   var resp = await response.json()
-  localStorage.setItem("options",resp.optionsHtml)
+ // localStorage.setItem("options",resp.optionsHtml)
   cargarListaGranjas(resp.optionsHtml)
-  document.getElementById("granja").value = codInput
-  document.getElementById("cod").value =  codInput
+ // document.getElementById("granja").value = codInput
+ // document.getElementById("cod").value =  codInput
 
-  if (resp.html == "NoAuth" || resp.html == "") {
+  if (resp.html == "NoAuth" ) {
       document.getElementById("elemento1").innerHTML = "<div style='color:red; text-align:center;'><br> INDICA UNA GRANJA Y CLAVE VALIDOS </div>"
       document.getElementById("tituloGranja").innerHTML = "PREMIER PIGS";
-      document.getElementById("granja").value =""
       document.getElementById("cod").value =""
       verLogForm()
-    } else {
-      document.getElementById("elemento1").innerHTML =resp.html
+    } else   if ( resp.html == "") {
+      document.getElementById("elemento1").innerHTML = "<div style=' text-align:center;'><br> INDICA UNA GRANJA Y CLAVE VALIDOS </div>"
+      document.getElementById("tituloGranja").innerHTML = "PREMIER PIGS";
+      document.getElementById("cod").value =""
+      verLogForm()
+    } else {     
+      document.getElementById("elemento1").innerHTML = resp.html
       document.getElementById("tituloGranja").innerHTML =  granjaInput.toUpperCase() 
       var valor = JSON.stringify({ granja: granjaInput, cod: codInput })
       localStorage.setItem('granja', valor);
